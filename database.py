7defdb88f335a9c612527334dc125875301ac904
Jens <95865104+jensbogen@users.noreply.github.com>
2023-10-31 09:12:26 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from neo4j import GraphDatabase, Driver
 
-URI = "neo4j+s://e478977f.databases.neo4j.io"
-AUTH = ("neo4j", "_T8z-J6M7BJxAL0QYGtaUQrX1YMSFE23J9N1BlgNEUY")
+URI = "uri"
+AUTH = ("username", "authkey")
 
 def _get_connection() -> Driver:
     driver = GraphDatabase.driver(URI, auth=AUTH)
